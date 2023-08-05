7e99437540aa3b39d1e1b598621807a0f18b2b47
slapthorn-hm <141457757+slapthorn-hm@users.noreply.github.com>
2023-08-05 14:29:41 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,13 +1,3 @@
-function calculatetimebetweentwodatetimes(start, end){
-    if (start > end) return false;
-    var start = new Date(start);
-    var end = new Date(end);
-    var diff = end - start;
-    var days = Math.floor(diff / 1000 / 60 / (60 * 24));
-    var hours = Math.floor(diff / 1000 / 60 / 60);
-    var minutes = Math.floor(diff / 1000 / 60);
-    var seconds = Math.floor(diff / 1000);
-    return {days: days, hours: hours, minutes: minutes, seconds: seconds};
+function calculateNumbers(var1, var2){
+    return var1 + var2;
 }
-
-calculatetimebetweentwodatetimes('2019-01-01 00:00:00', '2019-01-01 00:00:01');
